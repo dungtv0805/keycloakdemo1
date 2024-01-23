53d487ed8d8686d120761d4195e1d6631d282702
chimseo <dung.tv0805@gmail.com>
2024-01-23 15:47:25 +0700

    update

--- react/src/hooks/useAuth.jsx
+++ react/src/hooks/useAuth.jsx
@@ -16,24 +16,28 @@ const useAuth = () => {
     setToken(null);
     setLogin(false);
   }, [token]);
-
   useEffect(() => {
     if (isRun.current) return;
-
     isRun.current = true;
     client
       .init({
         onLoad: "check-sso",
+        redirectUri: "http://chimseoclient.com:5175/",
+        responseMode: "query",
+        // flow: "hybrid",
+        adapter: "default",
+        checkLoginIframe: false,
         silentCheckSsoRedirectUri:
           window.location.origin + "/silent-check-sso.html",
         pkceMethod: "S256",
       })
       .then((res) => {
         setLogin(res);
-        client.loadUserInfo();
-        if (res) {
-          setToken(client.token);
-        }
+        // client.loadUserInfo();
+        // if (res) {
+        //   console.log(client.token);
+        //   setToken(client.token);
+        // }
       });
   }, []);
   return [isLogin, token, clearToken];
--- react/vite.config.js
+++ react/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    port: 5175,
     proxy: {
       "/documents": "http://127.0.0.1:5000/",
     },
